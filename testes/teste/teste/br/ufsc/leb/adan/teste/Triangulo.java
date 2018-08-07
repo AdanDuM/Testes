f108d3b194ed477e51bb891f489307fd9affa06d
Adan Gomes <AdanDuM@users.noreply.github.com>
2018-08-07 17:38:26 -0300

    Commit inicial

---
+++ testes/teste/teste/br/ufsc/leb/adan/teste/Triangulo.java
@@ -0,0 +1,10 @@
+package teste.br.ufsc.leb.adan.teste;
+
+public class Triangulo {
+
+	public static boolean isTriangulo(int x, int y, int z) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
