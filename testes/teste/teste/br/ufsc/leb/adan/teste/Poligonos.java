f419d514a18818f401835d50324f8e33f5bcc095
AdanDuM <adandum@hotmail.com>
2018-08-09 17:28:27 -0300

    Feito correções sugeridas

---
+++ testes/teste/teste/br/ufsc/leb/adan/teste/Poligonos.java
@@ -0,0 +1,25 @@
+package teste.br.ufsc.leb.adan.teste;
+
+public class Poligonos {
+
+	public static boolean isTriangulo(Integer x, Integer y, Integer z) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public static boolean isIsoceles(Integer x, Integer y, Integer z) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	public static boolean isEscaleno(Integer x, Integer y, Integer z) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	public static boolean isEquilatero(Integer x, Integer y, Integer z) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
