d9516d2254fbda9b8ebd9ac6b5424b4114caa608
Douglass-MacBook-Air.local <Douglass-MacBook-Air.local@douglas>
2018-08-11 01:39:40 -0300

    Um pézinho na Orientação a Objetos

--- teste/teste/br/ufsc/leb/adan/teste/Triangulo.java
+++ teste/teste/br/ufsc/leb/adan/teste/Triangulo.java
@@ -3,8 +3,11 @@ package teste.br.ufsc.leb.adan.teste;
 public class Triangulo {
 
 	public static boolean isTriangulo(int x, int y, int z) {
-		// TODO Auto-generated method stub
 		return false;
+	}
+
+	public Boolean ehEquilatero() {
+		return Boolean.TRUE;
 	}
 
 }
---
+++ teste/teste/br/ufsc/leb/douglas/TrianguloEquilatero.java
@@ -0,0 +1,7 @@
+package teste.br.ufsc.leb.douglas;
+
+import teste.br.ufsc.leb.adan.teste.Triangulo;
+
+public class TrianguloEquilatero extends Triangulo {
+
+}
